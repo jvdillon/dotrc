54f95b043fbd4146d79cd4e6596d658d000551ad
Joshua V. Dillon <jvdillon@gmail.com>
2026-04-18 12:10:24 -0700

    add uvr

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@
 # Note: ubuntu defaults are:
 # /etc/skel/{.bash_logout,.bashrc,.profile}
 
-RCDIR=~/research/docs/dot-config
+RCDIR="$PWD"
 for f in ".bash_aliases" ".bashrc" ".gitconfig" ".inputrc" ".pystartup" ".screenrc" ".tmux.conf" ".vimrc" ; do
     ln -sf $RCDIR/$f ~/$f
 done
 mkdir -p ~/.config/nvim        && ln -sf $RCDIR/nvim_init.vim ~/.config/nvim/init.vim
-mkdir -p ~/.local/bin          && ln -sf $RCDIR/bc            ~/.local/bin/bc
-chmod 0744 ~/.local/bin/bc
+mkdir -p ~/.local/bin          && ln -sf $RCDIR/bc            ~/.local/bin/bc   && chmod 0744 ~/.local/bin/bc
+mkdir -p ~/.local/bin          && ln -sf $RCDIR/uvr           ~/.local/bin/uvr  && chmod 0744 ~/.local/bin/uvr
+
